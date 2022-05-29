1becc4c0f90586c08f0215082774b7dfdab27e66
HARFHO <Harfho77@gmail.com>
2022-05-29 10:41:03 +0100

    Create 2captcha.user.js

---
+++ 2captcha.user.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name         2captcha
+// @namespace    Acot
+// @version      0.2
+// @description  Solve Geetest v4 captcha with 2captcha service
+// @author       Acot
+// @updateURL    https://github.com/Acotec/autofcb_meta/raw/master/2captcha.user.js
+// @downloadURL  https://github.com/Acotec/autofcb_meta/raw/master/2captcha.user.js
+// @match        *://*/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=geetest.com
+// @grant        GM_setValue
+// @grant        GM_getValue
+// @grant        GM_addValueChangeListener
+// @grant        window.close
+// @grant        GM_setClipboard
+// @grant        GM_notification
+// @grant        GM_xmlhttpRequest
+// @grant        GM_getResourceText
+// @grant        GM_addElement
+// @grant        GM_registerMenuCommand
+// @run-at       document-start
+// @connect      2captcha.com
+// @noframes
+// @require      https://github.com/Acotec/aide_script/raw/master/2captcha.user.js
+// ==/UserScript==
