7fd80c8766db26a3acb37055717587c99604aa3a
HARFHO <39716223+Harfho@users.noreply.github.com>
2024-04-08 00:27:03 +0100

    Update 2Geetest.user.js

--- 2Geetest.user.js
+++ 2Geetest.user.js
@@ -4,8 +4,8 @@
 // @version      0.4
 // @description  Solve Geetest v4 captcha with 2captcha service
 // @author       Acot
-// @updateURL    https://github.com/Acotec/aide_script/raw/master/2Geetest.user.js
-// @downloadURL  https://github.com/Acotec/aide_script/raw/master/2Geetest.user.js
+// @updateURL    https://github.com/Acotec/aide_script/raw/root/2Geetest.user.js
+// @downloadURL  https://github.com/Acotec/aide_script/raw/root/2Geetest.user.js
 // @match        *://*/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=geetest.com
 // @grant        GM_setValue
@@ -24,5 +24,5 @@
 // @connect      gist.github.com
 // @connect      gist.githubusercontent.com
 // @noframes
-// @require      https://github.com/Acotec/aide_script/raw/master/2Geetest.user.js
+// @require      https://github.com/Acotec/aide_script/raw/root/2Geetest.user.js
 // ==/UserScript==
